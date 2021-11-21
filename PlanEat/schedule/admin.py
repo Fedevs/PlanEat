868b81e06750eb4f0c6a0e3d3a792ad039150875
Federico <federico.poncela@bigbox.com.ar>
2021-11-21 20:53:35 -0300

    models ready

--- PlanEat/schedule/admin.py
+++ PlanEat/schedule/admin.py
@@ -2,5 +2,22 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from schedule.models import Recipe, Meal, Ingredient
+
+class RecipeInline(admin.TabularInline):
+    model = Recipe
+    fields = ('meal', 'ingredient', 'quantity')
+
+class MealAdmin(admin.ModelAdmin):
+    fields = ('name', 'cooking_time', 'tags', 'day_time', )
+    inlines = [
+        RecipeInline,
+    ]
+
+class IngredientAdmin(admin.ModelAdmin):
+    fields = ('name',)
+
+admin.site.register(Meal, MealAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
 
 # Register your models here.
--- PlanEat/schedule/models.py
+++ PlanEat/schedule/models.py
@@ -1,36 +1,35 @@
 from django.db import models
 
-class Meals(models.model):
-    KIND_CHOICES = [
-        ('meat', 'Con carne'),
-        ('vegan', 'Vegana'),
-        ('veggie', 'vegetariana'),
-    ]
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Nombre',)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+class Meal(models.Model):
     DAY_TIME_CHOICES = [
-        ('BR', 'Desayuno'),
         ('LN', 'Almuerzo'),
-        ('TT', 'Merienda'),
         ('DN', 'Cena'),
         ('DL', 'Almuerzo/Cena'),
-        ('BT', 'Desayuno/Merienda'),
     ]
     name = models.CharField(max_length=50, verbose_name='Nombre')
-    ingredients = models.ManyToManyField(Ingredients, verbose_name='Ingredientes')
     cooking_time = models.CharField(max_length=10, verbose_name='Tiempo de cocción')
-    kind = models.CharField(
-        choices=KIND_CHOICES,
-        default='vegan',
-        verbose_name='Tipo de comida',
-    )
+    tags = models.CharField(max_length=30, verbose_name='Etiquetas')
     day_time = models.CharField(
+        max_length=2,
         choices=DAY_TIME_CHOICES,
         default='BR',
         verbose_name='Momento del día',
     )
-    recipe = models.CharField(null=True, blank=True, verbose_name='Receta')
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+class Recipe(models.Model):
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE,  verbose_name='Comida')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='Ingredients', verbose_name='Ingrediente')
+    quantity =   models.FloatField(verbose_name='Cantidad')
+
+    def __str__(self) -> str:
+        return f'{self.meal}'
 
 
-class Ingredients(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Nombre'),
-    meals = models.ForeignKey(Meals, on_delete=models.CASCADE),
-    quantity = models.IntegerField(verbose_name='Cantidad')
