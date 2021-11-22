e4ca8573d89e0b4c725ea242d79bcaff8b12a893
Federico <federico.poncela@bigbox.com.ar>
2021-11-22 16:46:15 -0300

    adding templates /schedule/

--- schedule/views.py
+++ schedule/views.py
@@ -1,6 +1,9 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+from .models import Ingredient
 
 # Create your views here.
+
+def schedule(request):
+    days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+    ingredients = Ingredient.objects.all()
+    return render(request, 'ingredients.html', {'ingredients': ingredients, 'days': days})
