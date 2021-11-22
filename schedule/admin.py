05d81cf8f183dfe22fe967d584c03bd9635c0884
Federico <federico.poncela@bigbox.com.ar>
2021-11-22 18:16:14 -0300

    category model added

--- schedule/admin.py
+++ schedule/admin.py
@@ -2,22 +2,27 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from schedule.models import Recipe, Meal, Ingredient
+from schedule.models import Recipe, Meal, Ingredient, Category
 
 class RecipeInline(admin.TabularInline):
     model = Recipe
     fields = ('meal', 'ingredient', 'quantity')
 
+
 class MealAdmin(admin.ModelAdmin):
-    fields = ('name', 'cooking_time', 'tags', 'day_time', )
+    fields = ('name', 'cooking_time', 'tags', 'day_time', 'category', )
     inlines = [
         RecipeInline,
     ]
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('name',)
 
 class IngredientAdmin(admin.ModelAdmin):
     fields = ('name',)
 
 admin.site.register(Meal, MealAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Category, CategoryAdmin)
 
 # Register your models here.
--- schedule/models.py
+++ schedule/models.py
@@ -5,24 +5,39 @@ class Ingredient(models.Model):
 
     def __str__(self) -> str:
         return f'{self.name}'
+
+
 class Meal(models.Model):
     DAY_TIME_CHOICES = [
         ('LN', 'Almuerzo'),
         ('DN', 'Cena'),
         ('DL', 'Almuerzo/Cena'),
     ]
+    TAG_CHOICES = [
+        ('VG','Vegano'),
+        ('VT','Vegetariano'),
+        ('OM','Omnívoro'),
+        ('CL','Celíaco'),
+    ]
     name = models.CharField(max_length=50, verbose_name='Nombre')
     cooking_time = models.CharField(max_length=10, verbose_name='Tiempo de cocción')
-    tags = models.CharField(max_length=30, verbose_name='Etiquetas')
+    tags = models.CharField(
+        max_length=2,
+        choices=TAG_CHOICES,
+        default='',
+        verbose_name='Etiquetas',
+    )
     day_time = models.CharField(
         max_length=2,
         choices=DAY_TIME_CHOICES,
         default='BR',
         verbose_name='Momento del día',
     )
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
 
     def __str__(self) -> str:
         return f'{self.name}'
+
 
 class Recipe(models.Model):
     meal = models.ForeignKey(Meal, on_delete=models.CASCADE,  verbose_name='Comida')
@@ -31,5 +46,13 @@ class Recipe(models.Model):
 
     def __str__(self) -> str:
         return f'{self.meal}'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Categoría')
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+    
 
 
