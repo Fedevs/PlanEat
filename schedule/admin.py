67e3e23922dec68dbb8ce832bf324d457df2ea42
Yani Echeva <yaniiecheva@gmail.com>
2022-03-25 17:49:19 -0300

    Model update and optimization

--- schedule/admin.py
+++ schedule/admin.py
@@ -2,33 +2,36 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from schedule.models import Recipe, Meal, Ingredient, Category
+from schedule.models import Recipe, Meal, Ingredient, Category, Schedule
+
 
 class RecipeInline(admin.TabularInline):
     model = Recipe
     fields = ('meal', 'ingredient', 'quantity')
-    raw_id_fields = ('ingredient',)
 
 
 class MealAdmin(admin.ModelAdmin):
-    fields = ('name', 'cooking_time', 'tags', 'day_time', 'category', )
     inlines = [
         RecipeInline,
     ]
-    raw_id_fields = ('category',)
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    fields = ('name',)
+    search_fields = ('name',)
 
 
 class IngredientAdmin(admin.ModelAdmin):
-    fields = ('name', 'measurement_unit')
     search_fields = ('name',)
+
+
+class ScheduleAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    filter_fields = ('date', 'meal_time',)
 
 
 admin.site.register(Meal, MealAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Schedule, ScheduleAdmin)
 
 # Register your models here.
