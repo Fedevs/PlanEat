d610cc9a51ac48e9d95d5951265ebc2ab4027f66
FedeG <federicogonzalez761@gmail.com>
2022-03-25 19:59:37 -0300

    Update scheduler logic

--- scheduler/schedule.py
+++ scheduler/schedule.py
@@ -1,41 +1,53 @@
-from os import name
 import calendar
-from django.shortcuts import render
-from .models import Meal, Schedule
-from random import choice, random
 from datetime import date, timedelta
+
+from django.db.models import Q
+
+from .models import Meal, Schedule
 # Create your views here.
 
-def schedule(request):
-    
-    lunch_meals = Meal.objects.exclude(day_time="DN").order_by("?")
-    dinner_meals = Meal.objects.exclude(day_time="LN").order_by("?")
-    
+def schedule():
     #TODO: revisar como empieza
     starting_date = date.today()
+    current_date = starting_date
     menu =[]
 
     days = calendar.day_name
-    for day in days:
-        #For lunch
-        lunch_meal = None
-        for meal in lunch_meals.iterator():
-            if meal_validity(meal):
-                lunch_meal = meal
-                break
-        schedule_ln = Schedule.objects.create(date=starting_date, meal_time="LN", meal=lunch_meal)
-        menu.append(schedule_ln)
-        dinner_meal = None
-        #For dinner 
-        for meal in dinner_meal.iterator():
-            if meal_validity(meal):
-                dinner_meal = meal
-                break
-        schedule_dn = Schedule.objects.create(date=starting_date, meal_time="DN", meal=dinner_meal)
-        menu.append(schedule_dn)
-        starting_date+= timedelta(days=1)
+    for _ in days:
+        if calendar.day_name[current_date.weekday()] == 'Monday':
+            break
 
+        for meal_time in ['LN', 'DN']:
+            if Schedule.objects.filter(
+                date=current_date, meal_time=meal_time, meal__isnull=False
+            ).exists():
+                continue
+
+            meal = None
+            candidate_meals = Meal.objects.filter(
+                Q(day_time=meal_time) | Q(day_time='DL')
+            ).order_by('?')
+
+            for candidate_meal in candidate_meals.iterator():
+                if meal_validity(candidate_meal, current_date, meal_time):
+                    meal = candidate_meal
+                    break
+
+            new_meal_scheduled, _ = Schedule.objects.update_or_create(
+                date=current_date, meal_time=meal_time, defaults={"meal": meal}
+            )
+            menu.append(new_meal_scheduled)
+
+        current_date += timedelta(days=1)
     return menu
 
-def meal_validity(meal):
-    return True
+def meal_validity(meal: Meal, current_date, meal_time):
+    previous_meals = Schedule.objects.filter(
+        date__lte=current_date
+    ).exclude(
+        date=current_date, meal_time=meal_time
+    ).order_by('-date', 'meal_time')
+    meals_in_frequency = previous_meals[:meal.category.frequency]
+    previous_categories = meals_in_frequency.values_list('meal__category__name', flat=True)
+
+    return not meal.category.name in previous_categories
