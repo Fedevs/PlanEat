c2935920ba5b48f18ca3b58e56eff07a1aeb5433
Yani Echeva <yaniiecheva@gmail.com>
2022-03-25 16:03:14 -0300

    Fix django imports for Django 4 urls: url -> path

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url('', views.schedule, name='schedule'),
+    path('', views.schedule, name='schedule'),
 ]
